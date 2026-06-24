b766af408395bbb7bc75b4237df1a72f51cb21fa
Jagriti301999 <priyadarshanijagriti@gmail.com>
2026-06-24 19:35:29 +0000

    c

--- Agentic_AI/Notebook/04_Query_VS_Index.py
+++ Agentic_AI/Notebook/04_Query_VS_Index.py
@@ -1,8 +1,4 @@
 # Databricks notebook source
- 
-
-# COMMAND ----------
-
 # DBTITLE 1,Install required packages
 # MAGIC %pip install databricks-vectorsearch
 
@@ -28,4 +24,21 @@ vsc = VectorSearchClient(
 
 index = vsc.get_index(endpoint_name="vs_endpoint_1", index_name="agentic_catalog.agentic_schema.product_docs_index")
 
-index.similarity_search(num_results=3, columns=["indexed_doc","product_id"], query_text="where can i find tutorial for AccountEase Pro  product", query_type="HYBRID")
+index.similarity_search(num_results=3, columns=["indexed_doc","product_id"], query_text="where can i find tutorial for AccountEase Pro  product in short ", query_type="HYBRID")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC [
+# MAGIC [NOTICE] Using a notebook authentication token. Recommended for development only. For improved performance, please use Service Principal based authentication. To disable this message, pass disable_notice=True.
+# MAGIC {'manifest': {'column_count': 3,
+# MAGIC   'columns': [{'name': 'indexed_doc'},
+# MAGIC    {'name': 'product_id'},
+# MAGIC    {'name': 'score'}]},
+# MAGIC  'result': {'row_count': 3,
+# MAGIC   'data_array': [["<product_category>Software</product_category>\n<product_sub_category>Online Platform</product_sub_category>\n<product_name>AccountEase Pro</product_name>\n<product_doc>\nAccountEase Pro Documentation\nProduct Overview\nAccountEase Pro is a robust online platform designed to simplify\npersonal account management for individuals and businesses alike.\nWith an intuitive interface, users can seamlessly manage their\naccounts, reset passwords, and maintain high security standards.\nGetting Started\nSign Up\nGo to the AccountEase Pro website.\nClick on the 'Sign Up' button.\nFill out the registration form with your name, email, and\npassword.\nConfirm your registration via the verification email.\nLogin\nAccess the portal through the homepage.\nEnter your registered email and password.\nClick 'Login'.\nNavigation\nThe dashboard features user-friendly tabs for quick access to\nyour profile, settings, and support.\nAccount Management\nResetting Your Password\nClick on 'Forgot Password?' on the login page.\nEnter your email address and submit the form.\nCheck your email for a reset link and follow the instructions.\nEnsure the link is not expired (valid for 24 hours).\nUpdating Profile Information1. \n2. \n3. \n4. \n5. \n6. \n7. \n8. \n9. \n10. \n11. \n1. \n2. \n3. \n4. \n5. \n6. \nNavigate to 'My Profile'.\nEdit fields such as your name, email, and phone number.\nSave changes.\nManaging Security Settings\nGo to 'Security Settings' in your account menu.\nEnable two-factor authentication for enhanced security.\nReview login history and change your password regularly.\nCommon Troubleshooting\nProblem: Cannot Reset Password\nEnsure the reset email hasn't gone to the spam folder.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
--- Agentic_AI/Notebook/05_Create_User_Defined_Functions.py
+++ Agentic_AI/Notebook/05_Create_User_Defined_Functions.py
@@ -30,8 +30,27 @@ CREATE OR REPLACE FUNCTION agentic_catalog.agentic_schema.get_return_policy(
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC |policy|policy_details|last_updated|
+# MAGIC |---|---|---|
+# MAGIC |Account Cancellation Policy|Customers have the right to cancel their accounts at any given time. Upon successful cancellation, a confirmation email will be sent to the registered email address. The account data will be retained for a period of 30 days post-cancellation for any potential follow-up actions.|2023-02-20|
+# MAGIC |Exchange Policy|Customers can exchange products for a similar item within 30 days of the original purchase date. The exchanged items must be in their original condition and packaging. Note that exchanges do not count towards the annual return limit of 12 items per customer.|2023-01-30|
+# MAGIC |Refund Policy|Refunds will be processed within 5-7 business days after the returned item has been received and inspected by our team. The refund will be issued to the original payment method used during the purchase. Customers will be notified via email once the refund has been processed.|2023-03-05|
+# MAGIC |Warranty Policy|All products are covered by a one-year warranty that protects against manufacturing defects. Customers can file a warranty claim within this period. The claimed items will be subject to inspection to determine eligibility for repair or replacement.|2023-02-10|
+# MAGIC |Privacy Policy|Customer data is safeguarded and will not be shared with third parties without explicit consent from the customer. Customers have the right to request the deletion of their data at any time. Data deletion requests will be processed within 30 days.|2023-01-25|
+# MAGIC |Return Policy|Customers are entitled to return 12 items per calendar year and each must be within a period of 30 days from the date of purchase.|2023-01-15|
+
+# COMMAND ----------
+
 # MAGIC %sql
 # MAGIC select * from agentic_catalog.agentic_schema.get_return_policy("Return Policy")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC |policy|policy_details|last_updated|
+# MAGIC |---|---|---|
+# MAGIC |Return Policy|Customers are entitled to return 12 items per calendar year and each must be within a period of 30 days from the date of purchase.|2023-01-15|
 
 # COMMAND ----------
 
@@ -59,9 +78,23 @@ CREATE OR REPLACE FUNCTION agentic_catalog.agentic_schema.get_service_history(
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC select * from agentic_catalog.agentic_schema.cust_service_data 
+# MAGIC select * from agentic_catalog.agentic_schema.cust_service_data limit 1
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC |customer_id|name|email|phone_number|address|interaction_id|date_time|issue_category|issue_description|agent_id|
+# MAGIC |---|---|---|---|---|---|---|---|---|---|
+# MAGIC |453e50e0-232e-44ea-9fe3-28d550be6294|Nicolas Pelaez|nicolas.pelaez@example.com|2327115355|219 Kevin Shores, New Melissafort, PW 78001|2b8f5009-c1ef-49ec-bc06-c2b3e170cfb3|2025-07-11T13:43:48.656+00:00|Returns|Hi, I've been enjoying my new SoundWave X5 Pro Headphones, but suddenly they're not connecting to my phone anymore. They don't show up in my Bluetooth devices list at all. It's frustrating because they were working perfectly yesterday. Is there a way to fix this, or should I proceed with a return?|38|
 
 # COMMAND ----------
 
 # MAGIC %sql
 # MAGIC select * from agentic_catalog.agentic_schema.get_service_history("nicolas.pelaez@example.com")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC |returns_last_12_months|issue_category|todays_date|
+# MAGIC |---|---|---|
+# MAGIC |23|Returns|2026-06-24|
